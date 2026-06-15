439e12d7fa581fc767a2d622ad47d49dfcb047e4
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-06-15 15:48:17 +0500

    feat: create high-conversion job portal engine with job filter matrices and details layout

---
+++ src/pages/JobPortal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JobPortalDashboard from "../components/jobportal/JobPortalDashboard";
+
+const JobPortal = () => {
+  return (
+    <>
+      <JobPortalDashboard />
+    </>
+  );
+};
+
+export default JobPortal;
